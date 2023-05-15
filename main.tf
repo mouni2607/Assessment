e93a57e284018c93f4360c88da1b1230957b1f96
mouni2607 <121868004+mouni2607@users.noreply.github.com>
2023-05-15 12:31:08 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
 }
 
 data "azurerm_resource_group" "RG" {
-  name     = "AssessmentRG"     //<env>-<app or service name>-rg-<region>-<###>
+  name     = "TerraformRG"     //<env>-<app or service name>-rg-<region>-<###>
   
 }
 
@@ -16,15 +16,15 @@ resource "azurerm_virtual_network" "Vn" {
 
 resource "azurerm_subnet" "subnet" {
   name                 = "snet-shared-EastUs-001"           //snet-<subscription purpose>-<region>-<###>
-  resource_group_name  = "${data.azurerm_resource_group.prod.name}"
+  resource_group_name  = "${data.azurerm_resource_group.RG.name}"
   virtual_network_name = azurerm_virtual_network.Vn.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_network_interface" "nic" {
   name                = "nic-01-Terra-shared-001"      //nic-<##>-<vm name>-<subscription purpose>-<###>
-  location            = azurerm_resource_group.RG.location
-  resource_group_name = "${data.azurerm_resource_group.prod.name}"
+  location            = "${data.azurerm_resource_group.RG.location}"
+  resource_group_name = "${data.azurerm_resource_group.RG.name}"
   ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet.id
@@ -34,8 +34,8 @@ resource "azurerm_network_interface" "nic" {
 
 resource "azurerm_windows_virtual_machine" "vm" {
   name                = "vm-Terra-001"                      //vm-<policy name or app name>-<###>
-  resource_group_name = "${data.azurerm_resource_group.prod.name}"
-  location            = "${data.azurerm_resource_group.prod.location}"
+  resource_group_name = "${data.azurerm_resource_group.RG.name}"
+  location            = "${data.azurerm_resource_group.RG.location}"
   size                = "Standard_F2"
   admin_username      = "Mounika"
   admin_password      = "qwer@1234567"
@@ -60,21 +60,21 @@ resource "azurerm_windows_virtual_machine" "vm" {
 resource "azurerm_virtual_network" "VN" {
   name                = "vnet-shared-EastUS-002"            //vnet-<subscription purpose>-<region>-<###>
   address_space       = ["10.0.0.0/16"]
-  location            = "${data.azurerm_resource_group.prod.location}"
-  resource_group_name = "${data.azurerm_resource_group.prod.name}"
+  location            = "${data.azurerm_resource_group.RG.location}"
+  resource_group_name = "${data.azurerm_resource_group.RG.name}"
 }
 
 resource "azurerm_subnet" "subnet1" {
   name                 = "snet--shared-EastUs-002"           //snet-<subscription purpose>-<region>-<###>
-  resource_group_name  = "${data.azurerm_resource_group.prod.name}"
+  resource_group_name  = "${data.azurerm_resource_group.RG.name}"
   virtual_network_name = azurerm_virtual_network.VN.name
   address_prefixes     = ["10.0.3.0/24"]
 }
 
 resource "azurerm_network_interface" "network" {
   name                = "nic-02-TFmach-shared-001"          //nic-<##>-<vm name>-<subscription purpose>-<###>
-  location            = "${data.azurerm_resource_group.prod.location}"
-  resource_group_name = "${data.azurerm_resource_group.prod.name}"
+  location            = "${data.azurerm_resource_group.RG.location}"
+  resource_group_name = "${data.azurerm_resource_group.RG.name}"
   ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet1.id
@@ -84,8 +84,8 @@ resource "azurerm_network_interface" "network" {
 
 resource "azurerm_windows_virtual_machine" "virtual" {
   name                = "vm-TFmach-001"                          //vm-<policy name or app name>-<###>
-  resource_group_name = "${data.azurerm_resource_group.prod.name}"
-  location            = "${data.azurerm_resource_group.prod.location}"
+  resource_group_name = "${data.azurerm_resource_group.RG.name}"
+  location            = "${data.azurerm_resource_group.RG.location}"
   size                = "Standard_F2"
   admin_username      = "Mounika"
   admin_password      = "qwer@1234567"
@@ -108,8 +108,8 @@ resource "azurerm_windows_virtual_machine" "virtual" {
 
 resource "azurerm_network_security_group" "NSG" {
   name                = "nsg-Networksecuritygroup-001"           ///nsg-<policy name or app name>-<###>
-  location            = "${data.azurerm_resource_group.prod.location}"
-  resource_group_name = "${data.azurerm_resource_group.prod.name}"
+  location            = "${data.azurerm_resource_group.RG.location}"
+  resource_group_name = "${data.azurerm_resource_group.RG.name}"
 }
 
 resource "azurerm_network_security_rule" "NSG" {
@@ -122,7 +122,7 @@ resource "azurerm_network_security_rule" "NSG" {
   destination_port_range      = "*"
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
-  resource_group_name         = "${data.azurerm_resource_group.prod.name}"
+  resource_group_name         = "${data.azurerm_resource_group.RG.name}"
   network_security_group_name = azurerm_network_security_group.NSG.name
 }
 
